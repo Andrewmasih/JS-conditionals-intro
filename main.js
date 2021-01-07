993a925d43de15b4c4a5d23a27208416cee67ab4
Andrew Masih <armasih@outlook.com>
2021-01-07 19:38:22 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let sky = "blue";
+
+if ( sky === "blue" ) { 
+	/* sky is blue, meaning the condition is TRUE.
+	 That is why console.log will run */
+  console.log("Nice weather");
+} else {
+	/* If sky was something other than blue, this code block would run */
+  console.log("Not nice weather");
+}
+
+/* We set the variable sky to blue. If the sky is blue, 
+then first console.log will run. Otherwise second console.log will run.
+Try changing sky to "gray" and see what happens */
